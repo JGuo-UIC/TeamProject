b62fcd77584c8f93aa35c49579b5254cfeb18c32
HarryL5004 <loklok8795@gmail.com>
2019-10-13 20:08:21 -0500

    Added additional test for Card

--- Baccarat_Project_Fall19/src/test/java/CardTest.java
+++ Baccarat_Project_Fall19/src/test/java/CardTest.java
@@ -6,7 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class CardTest {        
+class CardTest {
+    
+    @Test
+    void testinit() {
+        Card card1 = new Card("Spades", 1);
+        assertEquals("Card", card1.getClass().getName());
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12,13})
     void testSpadesCard(int arg) {
@@ -31,6 +38,22 @@ class CardTest {
         assertEquals("Clubs", card1.getSuite(), "GetSuite returned wrong suit");
         assertEquals(arg, card1.getValue(), "GetValue returned wrong value");
     }    
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12,13})
+    void testSetValue(int arg) {        
+        Card card1 = new Card("Spades", 1);
+        assertTrue(card1.setValue(arg), "SetValue returned false for valid value");
+        assertEquals(arg, card1.getValue(), "GetValue returned wrong value");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Spades", "Diamonds", "Clubs", "Hearts"})
+    void testSetSuite(String arg) {
+        Card card1 = new Card("Hearts", 1);        
+        assertTrue(card1.setSuite(arg), "SetSuite returned false for valid suit");
+        assertEquals(arg, card1.getSuite(), "GetSuite returned wrong suit");
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {0, -1, 1999999999, 14, 103, 1324, -12})
