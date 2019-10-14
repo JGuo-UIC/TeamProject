079f5ed2a28182075d6ebe8c33de1f2c5a0c43e5
JGuo-UIC <56518284+JGuo-UIC@users.noreply.github.com>
2019-10-14 17:48:04 -0500

    Add files via upload
    Finished test for GameLogic.

--- Baccarat_Project_Fall19/src/test/java/GameLogicTest.java
+++ Baccarat_Project_Fall19/src/test/java/GameLogicTest.java
@@ -1,11 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
+import org.junit.jupiter.api.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 
 class GameLogicTest {
@@ -22,8 +19,37 @@ class GameLogicTest {
     }
 
     @Test
-    void testWhoWon() {
-
+    void testWhoWonBanker() {
+        ArrayList<Card> hand1, hand2;
+        hand1 = new ArrayList<>();
+        hand2 = new ArrayList<>();
+        hand1.add(new Card("Spade", 2));
+        hand1.add(new Card("Spade", 3));
+        hand2.add(new Card("Spade", 5));
+        hand2.add(new Card("Spade", 3));
+        assertEquals("Banker", gLogic.whoWon(hand1, hand2), "Should return 'Banker'");
+    }
+    @Test
+    void testWhoWonPlayer() {
+        ArrayList<Card> hand1, hand2;
+        hand1 = new ArrayList<>();
+        hand2 = new ArrayList<>();
+        hand1.add(new Card("Spade", 5));
+        hand1.add(new Card("Spade", 3));
+        hand2.add(new Card("Spade", 3));
+        hand2.add(new Card("Spade", 3));
+        assertEquals("Player", gLogic.whoWon(hand1, hand2), "Should return 'Player'");
+    }
+    @Test
+    void testWhoWonTie() {
+        ArrayList<Card> hand1, hand2;
+        hand1 = new ArrayList<>();
+        hand2 = new ArrayList<>();
+        hand1.add(new Card("Spade", 3));
+        hand1.add(new Card("Diamond", 3));
+        hand2.add(new Card("Heart", 3));
+        hand2.add(new Card("Club", 3));
+        assertEquals("Draw", gLogic.whoWon(hand1, hand2), "Should return 'Draw'");
     }
 
     @Nested
@@ -58,13 +84,157 @@ class GameLogicTest {
         }
     }
 
-    @Test
-    void testEvaluateBankerDraw() {
+    @Nested
+    class testEvaluateBankerDraw {
+        ArrayList<Card> hand;
+        Card playerCard;
 
+        @BeforeEach
+        void setup(){
+            hand = new ArrayList<>();
+        }
+
+        @Test
+        void testZero() {
+            playerCard.setValue(5);
+            hand.add(new Card("Spade", 0));
+            hand.add(new Card("Diamond", 0));
+            assertTrue(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testOne(){
+            playerCard.setValue(5);
+            hand.add(new Card("Spade", 1));
+            hand.add(new Card("Diamond", 0));
+            assertTrue(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testTwo(){
+            playerCard.setValue(5);
+            hand.add(new Card("Spade", 1));
+            hand.add(new Card("Diamond", 1));
+            assertTrue(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testThreeDrew() {
+            playerCard.setValue(6);
+            hand.add(new Card("Spade", 1));
+            hand.add(new Card("Diamond", 2));
+            assertTrue(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testFourDrew() {
+            playerCard.setValue(6);
+            hand.add(new Card("Spade", 2));
+            hand.add(new Card("Diamond", 2));
+            assertTrue(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testFiveDrew() {
+            playerCard.setValue(6);
+            hand.add(new Card("Spade", 3));
+            hand.add(new Card("Diamond", 2));
+            assertTrue(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testSixDrew() {
+            playerCard.setValue(6);
+            hand.add(new Card("Spade", 3));
+            hand.add(new Card("Diamond", 3));
+            assertTrue(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testFourNDrew() {
+            playerCard.setValue(5);
+            hand.add(new Card("Spade", 2));
+            hand.add(new Card("Diamond", 2));
+            assertFalse(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testFiveNDrew() {
+            playerCard.setValue(5);
+            hand.add(new Card("Spade", 3));
+            hand.add(new Card("Diamond", 2));
+            assertFalse(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testSixNDrew() {
+            playerCard.setValue(5);
+            hand.add(new Card("Spade", 3));
+            hand.add(new Card("Diamond", 3));
+            assertFalse(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testSeven() {
+            playerCard.setValue(6);
+            hand.add(new Card("Spade", 3));
+            hand.add(new Card("Diamond", 4));
+            assertFalse(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testEight() {
+            playerCard.setValue(6);
+            hand.add(new Card("Spade", 4));
+            hand.add(new Card("Diamond", 4));
+            assertFalse(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
+        @Test
+        void testNine() {
+            playerCard.setValue(6);
+            hand.add(new Card("Spade", 5));
+            hand.add(new Card("Diamond", 4));
+            assertFalse(gLogic.evaluateBankerDraw(hand, playerCard));
+        }
     }
+    @Nested
+    class testEvaluatePlayerDraw{
+        ArrayList<Card> hand;
 
-    @Test
-    void testEvaluatePlayerDraw() {
-
-    }    
+        @BeforeEach
+        void setup(){
+            hand = new ArrayList<>();
+        }
+        @Test
+        void testZero(){
+            hand.add(new Card("Spade", 0));
+            hand.add(new Card("Diamond", 0));
+            assertTrue(gLogic.evaluatePlayerDraw(hand));
+        }
+        @Test
+        void testOne(){
+            hand.add(new Card("Spade", 1));
+            hand.add(new Card("Diamond", 0));
+            assertTrue(gLogic.evaluatePlayerDraw(hand));
+        }
+        @Test
+        void testTwo(){
+            hand.add(new Card("Spade", 1));
+            hand.add(new Card("Diamond", 1));
+            assertTrue(gLogic.evaluatePlayerDraw(hand));
+        }
+        @Test
+        void testThree(){
+            hand.add(new Card("Spade", 2));
+            hand.add(new Card("Diamond", 1));
+            assertTrue(gLogic.evaluatePlayerDraw(hand));
+        }
+        @Test
+        void testFour(){
+            hand.add(new Card("Spade", 2));
+            hand.add(new Card("Diamond", 2));
+            assertTrue(gLogic.evaluatePlayerDraw(hand));
+        }
+        @Test
+        void testFive(){
+            hand.add(new Card("Spade", 3));
+            hand.add(new Card("Diamond", 2));
+            assertTrue(gLogic.evaluatePlayerDraw(hand));
+        }
+        @Test
+        void testSix(){
+            hand.add(new Card("Spade", 3));
+            hand.add(new Card("Diamond", 3));
+            assertFalse(gLogic.evaluatePlayerDraw(hand));
+        }
+    }
 }
