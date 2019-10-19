87e585f8286e352ccf3d38ee010eca491a5120c7
HarryL5004 <loklok8795@gmail.com>
2019-10-18 21:02:16 -0500

    Added tests for evaluateWinnings

--- Baccarat_Project_Fall19/src/main/java/BaccaratGame.java
+++ Baccarat_Project_Fall19/src/main/java/BaccaratGame.java
@@ -1,59 +1,49 @@
-import java.awt.*;
-import java.awt.Label;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import com.sun.prism.paint.ImagePattern;
 import javafx.animation.PauseTransition;
 import javafx.animation.SequentialTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.effect.BlurType;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.*;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import javax.imageio.ImageIO;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BaccaratGame extends Application {
 	ArrayList<Card> playerHand;
 	ArrayList<Card> bankerHand;
 
-	BaccaratDealer theDealer = new BaccaratDealer();
-	BaccaratGameLogic gameLogic = new BaccaratGameLogic();
+	BaccaratDealer theDealer;
+	BaccaratGameLogic gameLogic;
 
-	String choice = "";
+	String choice;
 	double currentBet;
 	double totalWinnings;
 	boolean playerWin;
 
-	MenuBar menuBar = new MenuBar();
-    MenuItem frshStart;
-    HashMap<String, Scene> sceneMap = new HashMap<String, Scene>();
+	MenuBar menuBar;
+	MenuItem frshStart;
+	HashMap<String, Scene> sceneMap;
 
 	// Left VBox
 	TextField betMoney;
@@ -62,7 +52,7 @@ public class BaccaratGame extends Application {
 	ToggleGroup toggleGrp;
 	HBox betChoices;
 	Button playBtn;
-	TextArea result = new TextArea();
+	TextArea result;
 	TextField currWinnings;
 
 	// Right VBox
@@ -86,11 +76,11 @@ public class BaccaratGame extends Application {
 			else
 				resultWinnings = totalWinnings + currentBet * 8;
 			if (totalWinnings > resultWinnings) // deals with overflow
-			    resultWinnings = Double.MAX_VALUE;
+				resultWinnings = Double.MAX_VALUE;
 		} else {
 			resultWinnings = totalWinnings - currentBet;
 			if (totalWinnings < resultWinnings) // deals with overflow
-			    resultWinnings = Double.MIN_VALUE;
+				resultWinnings = Double.MIN_VALUE;
 		}
 		totalWinnings = resultWinnings;
 
@@ -106,12 +96,11 @@ public class BaccaratGame extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Let's Play Baccarat!!!");
 
-		// initialize the necessary attributes for the game
 		theDealer = new BaccaratDealer();
 		gameLogic = new BaccaratGameLogic();
 		sceneMap = new HashMap<String, Scene>();
-		menuBar = new MenuBar();
 		result = new TextArea();
+		menuBar = new MenuBar();
 		choice = "";
 		initMenu(primaryStage);
 		startGame(primaryStage);
@@ -298,11 +287,11 @@ public class BaccaratGame extends Application {
 		HBox playHBox = new HBox(playBtn);
 		playHBox.setAlignment(Pos.BOTTOM_CENTER);
 
-        result.setEditable(false); // set result textbox to be uneditable
+		result.setEditable(false); // set result textbox to be uneditable
 
 		// VBox that contains all of the child nodes in the left sidebar
-        VBox selection = new VBox(10, logo, betRow, betChoices, startHBox, result, playHBox);
-        // VBox styles
+		VBox selection = new VBox(10, logo, betRow, betChoices, startHBox, result, playHBox);
+		// VBox styles
 		selection.setMaxWidth(316.7);
 		selection.setStyle("-fx-background-color: #d32f2f;");
 		DropShadow vBoxDS = new DropShadow();
@@ -323,51 +312,51 @@ public class BaccaratGame extends Application {
 		GridPane board = new GridPane();
 
 		// Labels Player and Banker, Changes color of each letter and fonts
-        Text playerTextP = new Text("P");
-        playerTextP.setFill(Color.web("#ffc400"));
-        Text playerTextL = new Text("L");
-        playerTextL.setFill(Color.web("#ffc400"));
-        Text playerTextA = new Text("A");
-        playerTextA.setFill(Color.web("#ffc400"));
-        Text playerTextY = new Text("Y");
-        playerTextY.setFill(Color.web("#ffc400"));
-        Text playerTextE = new Text("E");
-        playerTextE.setFill(Color.web("#ffc400"));
-        Text playerTextR = new Text("R");
-        playerTextR.setFill(Color.web("#ffc400"));
-        //Add all the letter vertically
-        VBox playerText = new VBox(playerTextP,playerTextL,playerTextA,playerTextY,playerTextE,playerTextR);
-        playerText.setStyle("-fx-font-family: Algerian;" +
-                "-fx-font-size: 35;");
-        playerText.setPadding(new Insets(0,10,0,0));
+		Text playerTextP = new Text("P");
+		playerTextP.setFill(Color.web("#ffc400"));
+		Text playerTextL = new Text("L");
+		playerTextL.setFill(Color.web("#ffc400"));
+		Text playerTextA = new Text("A");
+		playerTextA.setFill(Color.web("#ffc400"));
+		Text playerTextY = new Text("Y");
+		playerTextY.setFill(Color.web("#ffc400"));
+		Text playerTextE = new Text("E");
+		playerTextE.setFill(Color.web("#ffc400"));
+		Text playerTextR = new Text("R");
+		playerTextR.setFill(Color.web("#ffc400"));
+		//Add all the letter vertically
+		VBox playerText = new VBox(playerTextP,playerTextL,playerTextA,playerTextY,playerTextE,playerTextR);
+		playerText.setStyle("-fx-font-family: Algerian;" +
+				"-fx-font-size: 35;");
+		playerText.setPadding(new Insets(0,10,0,0));
 
-        Text bankerTextB = new Text("B");
-        bankerTextB.setFill(Color.web("#ffc400"));
-        Text bankerTextA = new Text("A");
-        bankerTextA.setFill(Color.web("#ffc400"));
-        Text bankerTextN = new Text("N");
-        bankerTextN.setFill(Color.web("#ffc400"));
-        Text bankerTextK = new Text("K");
-        bankerTextK.setFill(Color.web("#ffc400"));
-        Text bankerTextE = new Text("E");
-        bankerTextE.setFill(Color.web("#ffc400"));
-        Text bankerTextR = new Text("R");
-        bankerTextR.setFill(Color.web("#ffc400"));
-        //Add all the letter vertically
-        VBox bankerText = new VBox(bankerTextB,bankerTextA,bankerTextN,bankerTextK,bankerTextE,bankerTextR);
-        bankerText.setStyle("-fx-font-family: Algerian;" +
-                "-fx-font-size: 35;");
-        bankerText.setPadding(new Insets(0,10,0,0));
+		Text bankerTextB = new Text("B");
+		bankerTextB.setFill(Color.web("#ffc400"));
+		Text bankerTextA = new Text("A");
+		bankerTextA.setFill(Color.web("#ffc400"));
+		Text bankerTextN = new Text("N");
+		bankerTextN.setFill(Color.web("#ffc400"));
+		Text bankerTextK = new Text("K");
+		bankerTextK.setFill(Color.web("#ffc400"));
+		Text bankerTextE = new Text("E");
+		bankerTextE.setFill(Color.web("#ffc400"));
+		Text bankerTextR = new Text("R");
+		bankerTextR.setFill(Color.web("#ffc400"));
+		//Add all the letter vertically
+		VBox bankerText = new VBox(bankerTextB,bankerTextA,bankerTextN,bankerTextK,bankerTextE,bankerTextR);
+		bankerText.setStyle("-fx-font-family: Algerian;" +
+				"-fx-font-size: 35;");
+		bankerText.setPadding(new Insets(0,10,0,0));
 		board.setAlignment(Pos.CENTER);
 
 		// initializes imageViews to hold the images of the banker's cards
-        bankerCard1 = new ImageView();
-        bankerCard1.setPreserveRatio(true);
+		bankerCard1 = new ImageView();
+		bankerCard1.setPreserveRatio(true);
 		bankerCard1.setFitHeight(250);
-        bankerCard2 = new ImageView();
+		bankerCard2 = new ImageView();
 		bankerCard2.setPreserveRatio(true);
 		bankerCard2.setFitHeight(250);
-        bankerCard3 = new ImageView();
+		bankerCard3 = new ImageView();
 		bankerCard3.setPreserveRatio(true);
 		bankerCard3.setFitHeight(250);
 		bankerPos = new HBox(bankerText, bankerCard1, bankerCard2, bankerCard3);
@@ -375,13 +364,13 @@ public class BaccaratGame extends Application {
 		bankerPos.setMargin(bankerCard2, new Insets(0,10,0,0));
 
 		// initializes imageViews to hold the images of the player's cards
-        playerCard1 = new ImageView();
+		playerCard1 = new ImageView();
 		playerCard1.setPreserveRatio(true);
 		playerCard1.setFitHeight(250);
-        playerCard2 = new ImageView();
+		playerCard2 = new ImageView();
 		playerCard2.setPreserveRatio(true);
 		playerCard2.setFitHeight(250);
-        playerCard3 = new ImageView();
+		playerCard3 = new ImageView();
 		playerCard3.setPreserveRatio(true);
 		playerCard3.setFitHeight(250);
 		playerPos.setMargin(playerCard1, new Insets(0,10,0,0));
@@ -400,8 +389,8 @@ public class BaccaratGame extends Application {
 	 * gamePlay runs the game with the game logic and deals with the transitions of the cards' images
 	 */
 	private void gamePlay() {
-        frshStart.setDisable(true); // disable fresh start button
-        SequentialTransition sT; // sequential transition to play pauseTransitions sequentially
+		frshStart.setDisable(true); // disable fresh start button
+		SequentialTransition sT; // sequential transition to play pauseTransitions sequentially
 		// pauseTransitions for displaying the cards
 		PauseTransition pause1 = new PauseTransition(Duration.seconds(2));
 		PauseTransition pause2 = new PauseTransition(Duration.seconds(2));
@@ -476,12 +465,12 @@ public class BaccaratGame extends Application {
 			playerWin = false;
 		currWinnings.setText(Double.toString(evaluateWinnings())); // displays resulted total winnings
 		playBtn.setDisable(false); // enable replay
-        frshStart.setDisable(false); // enable fresh start
+		frshStart.setDisable(false); // enable fresh start
 	}
 
 	/*
-	* gameEndMsg returns the text representation of end results
-	*/
+	 * gameEndMsg returns the text representation of end results
+	 */
 	private String gameEndMsg(String winner) {
 		String playerMsg = "Player Total: " + gameLogic.handTotal(playerHand);
 		String bankerMsg = " Banker Total: " + gameLogic.handTotal(bankerHand) + "\n";
@@ -513,5 +502,5 @@ public class BaccaratGame extends Application {
 				return (new Image(val + "C.png"));
 		}
 		return null;
-    }
+	}
 }
